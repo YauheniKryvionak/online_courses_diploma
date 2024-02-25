5bc12accfd62aa558baf8cb7869d63d099da54aa
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-02-25 14:23:02 +0300

    created Module model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -32,3 +32,17 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.title
