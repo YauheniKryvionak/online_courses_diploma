b0c0dbd7cbb43f5c6398d5ec1b2a2998a4369f0b
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-02-25 22:25:11 +0300

    imported edit views and reverse_laze

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Course
 
 
