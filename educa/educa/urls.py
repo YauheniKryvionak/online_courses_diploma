62fd0d577919b756d2218b00d6b3034562a92426
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-02-26 13:01:42 +0300

    fixed letter-mistake

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(),
          name='logout'),
     path('admin/', admin.site.urls),
-    path('course/', include('course.urls')),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
