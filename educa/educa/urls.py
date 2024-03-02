8a5c3be2e266a592e21acffecc6ef3a805c62287
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-03-02 11:48:57 +0300

    added comments

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -4,12 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+#  список URL-адресов
 urlpatterns = [
+    # Определяем URL-адрес для входа в систему
     path('accounts/login/', auth_views.LoginView.as_view(),
          name='login'),
+    # Определяем URL-адрес для выхода из системы
     path('accounts/logout/', auth_views.LogoutView.as_view(),
          name='logout'),
+    # Определяем URL-адрес для административной панели Django
     path('admin/', admin.site.urls),
+    # Определяем URL-адрес для курсов
     path('course/', include('courses.urls')),
 ]
 
