1214499ef7b32f2e71a2c54592cd58d63ad0c16c
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-03-02 10:24:53 +0300

    added comments for filter-module

--- educa/courses/templatetags/course.py
+++ educa/courses/templatetags/course.py
@@ -1,11 +1,15 @@
 from django import template
 
+# Импорт библиотеки шаблонов Django
 register = template.Library()
 
 
+# Фильтр для получения имени модели объекта
 @register.filter
 def model_name(obj):
     try:
+        # Пытается получить имя модели объекта
         return obj._meta.model_name
     except AttributeError:
+        # Если объект не является экземпляром модели, возвращает None
         return None
