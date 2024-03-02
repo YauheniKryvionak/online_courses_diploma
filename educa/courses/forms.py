91897002362fd99e70b6a1d19852767d4f023e44
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-03-02 10:49:00 +0300

    added comments for func and fields

--- educa/courses/forms.py
+++ educa/courses/forms.py
@@ -2,9 +2,12 @@ from django import forms
 from django.forms.models import inlineformset_factory
 from .models import Course, Module
 
+# Функция inlineformset_factory используется для создания класса ModuleFormSet,
+# который представляет собой набор форм для модели Module, которые будут встроены в форму для модели Course.
 ModuleFormSet = inlineformset_factory(Course,
                                       Module,
                                       fields=['title',
-                                              'description'],
-                                      extra=2,
-                                      can_delete=True)
+                                              'description'],  # Поля, которые должны быть представлены в форме
+                                      extra=2,  # Количество дополнительных пустых форм
+                                      can_delete=True)  # Флаг, указывающий, должна ли форма иметь возможность
+                                                        # удаления связанных объектов
