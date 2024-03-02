c414b7ea92b7d3bd0b8df08b00ccbf09601929df
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-03-02 10:33:32 +0300

    added comments for class

--- educa/courses/apps.py
+++ educa/courses/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
+# Конфигурация приложения 'courses'
 class CoursesConfig(AppConfig):
+    # Указываем, какое поле будет использоваться по умолчанию для автоматически генерируемых первичных ключей
     default_auto_field = 'django.db.models.BigAutoField'
+    # Указываем полное Python-имя приложения
     name = 'courses'
