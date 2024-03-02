5acd01c416d3f8d5e98416d196debfd525bed051
Yauheni Kryvionak <yauheni.kryvionak.dev@gmail.com>
2024-03-02 10:31:41 +0300

    added comments for classes

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -2,22 +2,30 @@ from django.contrib import admin
 from .models import Subject, Course, Module
 
 
-# Register your models here.
+# Регистрация моделей в админ-панели
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке предметов
     list_display = ['title', 'slug']
+    # Автоматически генерируем slug из названия предмета
     prepopulated_fields = {'slug': ('title',)}
 
 
+# Встроенный интерфейс для модулей внутри страницы редактирования курса
 class ModuleInline(admin.StackedInline):
     model = Module
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке курсов
     list_display = ['title', 'subject', 'created']
+    # Доступные фильтры
     list_filter = ['created', 'subject']
+    # Поля для поиска
     search_fields = ['title', 'overview']
+    # Автоматически генерируем slug из названия курса
     prepopulated_fields = {'slug': ('title',)}
+    # Встроенный интерфейс для модулей
     inlines = [ModuleInline]
